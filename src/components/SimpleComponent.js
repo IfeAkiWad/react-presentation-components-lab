96b28daa125ab822fc09d20f17acf9ebbff0a50d
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-09-01 21:13:50 -0500

    commit tree deaacf12a038fa02a4f9160d2479da56af32a231

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -10,17 +10,52 @@ export default class SimpleComponent extends Component {
     }
 
     handleClick = () => {
-        this.setState((previousState) => {
-           return {mood: !previousState.mood}
+        const toggleMood = this.state.mood == "happy" ? "sad" : "happy"
+        this.setState({
+            mood: toggleMood
         })
     }
+
+    // handleToggle = () => {
+    //     if (this.state.mood == "happy") {
+    //         return true
+    //     }
+    //     else if(this.state.mood == "sad") {
+    //         return false
+    //     }
+    // }
 
     render() {
         return (
             <div onClick={this.handleClick}>
-                <button>{this.state.mood ? "happy" : "sad"}</button>
-                
+                {this.state.mood}
             </div>
         )
     }
 }
+
+// import React, {Component} from 'react'
+
+// class SimpleComponent extends Component {
+//     state={
+//         mood: "happy"
+//     }
+
+//     handleClick = () => {
+//         const changeMood = this.state.mood === "happy" ? "sad" : "happy";
+//         this.setState({
+//             mood: changeMood
+//         })
+
+//     }
+
+//     render(){
+//         return(
+//             <div onClick={this.handleClick}>
+//             {this.state.mood}
+//             </div> 
+//         )
+//     }
+// }
+
+// export default SimpleComponent;
